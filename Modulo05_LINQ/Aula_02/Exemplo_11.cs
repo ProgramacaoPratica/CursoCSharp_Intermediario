bee0f6d86a812f0d7c0a7d3f739b83f81d3da5c6
Canal Programação Prática <49441314+ProgramacaoPratica@users.noreply.github.com>
2023-07-14 20:53:40 -0300

    Create Exemplo_11.cs

---
+++ Modulo05_LINQ/Aula_02/Exemplo_11.cs
@@ -0,0 +1,41 @@
+namespace ProjetoExemplo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lista1 = new List<int>() { 1, 2, 3, 4, 5 };
+            List<int> lista2 = new List<int>() { 4, 5, 6, 7, 8 };
+
+            var numerosDistintos = lista1.Distinct();
+            var uniao = lista1.Union(lista2);
+            var intersecao = lista1.Intersect(lista2);
+            var diferenca = lista1.Except(lista2);
+
+            Console.WriteLine("Números distintos:");
+            foreach (var numero in numerosDistintos)
+            {
+                Console.WriteLine(numero);
+            }
+
+            Console.WriteLine("União de ambas as listas:");
+            foreach (var numero in uniao)
+            {
+                Console.WriteLine(numero);
+            }
+
+            Console.WriteLine("Interseção entre as listas:");
+            foreach (var numero in intersecao)
+            {
+                Console.WriteLine(numero);
+            }
+
+            Console.WriteLine("Diferença entre as listas (lista1 - lista2):");
+            foreach (var numero in diferenca)
+            {
+                Console.WriteLine(numero);
+            }
+
+        }
+    }
+}
