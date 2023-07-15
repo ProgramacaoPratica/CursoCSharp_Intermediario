7dd7750e0991f9e11d740e6c6f20cf3e1ba9245e
Canal Programação Prática <49441314+ProgramacaoPratica@users.noreply.github.com>
2023-07-14 21:22:09 -0300

    Create Exemplo_03.cs

---
+++ Modulo04/Aula_01/Exemplo_03.cs
@@ -0,0 +1,27 @@
+namespace ProjetoExemplo
+{
+    public class Veiculo
+    {
+        public void Mover()
+        {
+            Console.WriteLine("O veículo está se movendo.");
+        }
+    }
+
+    public class Carro : Veiculo
+    {
+        public void Acelerar()
+        {
+            Console.WriteLine("O carro está acelerando.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var carro = new Carro();
+            carro.Acelerar();
+        }
+    }
+}
