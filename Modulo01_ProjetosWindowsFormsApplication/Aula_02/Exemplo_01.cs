62fa8c8ae5a7756042c552f11c00a4d966163ebb
Canal Programação Prática <49441314+ProgramacaoPratica@users.noreply.github.com>
2023-07-14 22:22:05 -0300

    Create Exemplo_01.cs

---
+++ Modulo01_ProjetosWindowsFormsApplication/Aula_02/Exemplo_01.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Windows.Forms;
+
+namespace MeuProjetoWindowsFormsApp
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+    }
+}
