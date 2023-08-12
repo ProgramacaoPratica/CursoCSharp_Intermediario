6ef54592452bd05655614d91284beb9bd2bd6cd5
Canal Programação Prática <49441314+ProgramacaoPratica@users.noreply.github.com>
2023-08-12 15:34:39 -0300

    Update Exemplo_01.cs

--- Modulo05_LINQ/Aula_02/Exemplo_01.cs
+++ Modulo05_LINQ/Aula_02/Exemplo_01.cs
@@ -4,7 +4,7 @@ namespace ProjetoExemplo
     {
         static void Main(string[] args)
         {
-            //Temos uma lista de números inteiros e queremos filtrar apenas os números pares
+            //Filtragem de uma lista: Suponha que temos uma lista de números inteiros e queremos filtrar apenas os números pares
             List<int> numeros = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             var numerosPares = numeros.Where(n => n % 2 == 0);
             foreach (var numero in numerosPares)
