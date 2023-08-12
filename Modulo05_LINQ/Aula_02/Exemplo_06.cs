c2e80299a338dc692fee4452c632049672a7de77
Canal Programação Prática <49441314+ProgramacaoPratica@users.noreply.github.com>
2023-08-12 15:37:29 -0300

    Update Exemplo_06.cs

--- Modulo05_LINQ/Aula_02/Exemplo_06.cs
+++ Modulo05_LINQ/Aula_02/Exemplo_06.cs
@@ -4,6 +4,7 @@ namespace ProjetoExemplo
     {
         static void Main(string[] args)
         {
+            //Consulta com agregação (Aggregate): Suponha que temos uma lista de números inteiros e queremos obter a soma de todos os números
             List<int> numeros = new List<int>() { 1, 2, 3, 4, 5 };
 
             int soma = numeros.Aggregate((acc, num) => acc + num);
