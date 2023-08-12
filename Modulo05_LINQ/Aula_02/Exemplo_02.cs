d3011d8f167a6fc4bef1758ac6eda754715d71be
Canal Programação Prática <49441314+ProgramacaoPratica@users.noreply.github.com>
2023-08-12 15:34:58 -0300

    Update Exemplo_02.cs

--- Modulo05_LINQ/Aula_02/Exemplo_02.cs
+++ Modulo05_LINQ/Aula_02/Exemplo_02.cs
@@ -4,6 +4,7 @@ namespace ProjetoExemplo
     {
         static void Main(string[] args)
         {
+            //Ordenação de uma lista: Suponha que temos uma lista de nomes e queremos ordená-los em ordem alfabética:
             List<string> nomes = new List<string>() { "João", "Maria", "Pedro", "Ana", "Lucas" };
             var nomesOrdenados = nomes.OrderBy(n => n);
             foreach (var nome in nomesOrdenados)
